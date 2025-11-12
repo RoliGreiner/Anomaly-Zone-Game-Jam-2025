cb0ccda0cc6e080bf62bc1cf5d0b95e81a29c11f
rreforged <90756889+rreforged@users.noreply.github.com>
2025-11-12 20:58:21 +0100

    Added a Main Menu

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var player_position: Vector2 = Vector2.ONE
+var volume := 0.5 	 # 0.0 - 1.0 tartományban mozog
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,3 +8,29 @@ func _on_start_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	# Főmenüből Optionsbe
+	$Main.visible = false
+	$OptionsMenu.visible = true
+
+
+func _on_button_pressed() -> void:
+	# Optionsből Főmenübe
+	$OptionsMenu.visible = false
+	$Main.visible = true
+
+
+func _on_toggle_fullscreen_pressed() -> void:
+	# Toggle Fullscreen
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	# Volume érték változtató (global.gd)
+	Global.volume = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
